3c1f8567380ba07c51e99a586398112a6b6ef934
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-09 19:07:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def batch_badge_creator(names)
+  newArray = []
+  for i in (0..names.size-1)
+    newArray[i] = badge_maker(names[i])
+  end 
+  return newArray
+end 
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def assign_rooms(names)
+  newArray = []
+  for i in (0..names.size-1)
+    newArray[i] = "Hello, #{names[i]}! You'll be assigned to room #{i+1}!"
+  end
+  return newArray
+end 
+
